0a6da9e5313c151e2c764af364789be03618398a
Adam Burstyn <adam.burstyn@gmail.com>
2021-06-15 15:40:24 -0400

    cleaned up code

--- NFL_Play_Prediction.py
+++ NFL_Play_Prediction.py
@@ -1,30 +1,13 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.metrics import accuracy_score
 import streamlit as st
 
 # Upload the data only when first opening the app
 flag = False
 if flag == False:
-    #plays = pd.read_csv("/Users/adamburstyn/Documents/Football Data/Play_Prediction/NFL_2017_Plays.csv")
     plays = pd.read_csv("NFL_2017_Plays.csv")
-
-
-    # # Remove unncesary columns
-    # plays = plays_all[["game_id", "game_date", "posteam", "defteam", "yardline_100", 
-    #                     "game_seconds_remaining", "down", "ydstogo", "shotgun", "no_huddle",
-    #                     "two_point_attempt", "score_differential_post", 
-    #                     "posteam_timeouts_remaining", "defteam_timeouts_remaining", "play_type"]]
-
-    # # Remove 2-point conversion plays as these should be analyzed seperately
-    # plays = plays.loc[plays["two_point_attempt"]!=1]
-    # plays = plays.drop(columns=["two_point_attempt"])
-
-    # # Keep only remaining run, pass, field goal, and punt plays
-    # plays = plays.loc[(plays["play_type"]=="run") | (plays["play_type"]=="pass") | 
-    #                 (plays["play_type"]=="punt") | (plays["play_type"]=="field_goal")]
 
     # Get a list of the teams
     team_list = plays["posteam"].sort_values().unique()
@@ -88,10 +71,6 @@ def user_input():
 
 play_input = user_input()
 
-# # Look at only games for a single season
-# season = play_input["season"].values[0]
-# season_plays = plays.loc[(plays["game_date"]>f"{season}-06-01") & (plays["game_date"]<f"{season+1}-06-01")]
-
 # Look at plays for a single team
 team = play_input["team"].values[0]
 team_plays = plays.loc[plays["posteam"]==team]
@@ -107,11 +86,6 @@ y = team_plays.iloc[:,13].values
 # Split the data into train and sets sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-# Standardize the independent variables
-sc = StandardScaler()
-#X_train = sc.fit_transform(X_train)
-#X_test = sc.fit_transform(X_test)
-
 # Create and train the random forrest model
 classifier = RandomForestClassifier(n_estimators=20, random_state=0)
 classifier.fit(X_train, y_train)
@@ -126,9 +100,7 @@ st.subheader("Game situation to predict:")
 st.write(play_input)
 
 # Get play prediction
-###### Need the proper transformation ##########
 X_predict = play_input.iloc[:,1:].values
-#X_predict = sc.fit_transform(X_predict)
 play_prediction = classifier.predict(X_predict)
 
 # Display predicted play
